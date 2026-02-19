e5cf881a83bde065ce4133f1586035aa86f9b41a
Grigoris Vgontzas <gregoryvgo@gmail.com>
2026-02-19 10:06:50 +0200

    fix export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Απαραίτητο για το Cloudflare Pages
+  images: {
+    unoptimized: true, // Απαραίτητο για να παίζουν οι εικόνες
+  },
 };
 
 export default nextConfig;
